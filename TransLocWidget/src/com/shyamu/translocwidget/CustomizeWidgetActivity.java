174a60eca09affb695443b6df52694655645bcd7
Shyamal Patel <shyamal.patel14@gmail.com>
2013-09-15 01:17:55 -0400

    added preview and color picker

---
+++ TransLocWidget/src/com/shyamu/translocwidget/CustomizeWidgetActivity.java
@@ -0,0 +1,19 @@
+package com.shyamu.translocwidget;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.widget.Toast;
+
+
+public class CustomizeWidgetActivity extends PreferenceActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.color_prefs);
+
+        Toast.makeText(getApplicationContext(),"Press back to save",Toast.LENGTH_LONG).show();
+
+
+    }
+}
