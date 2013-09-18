5579bd7819521ef3156c6b7ae3222858a0af1ab4
Shyamal Patel <shyamal.patel14@gmail.com>
2013-09-18 15:23:05 -0400

    changes to launcher activity

--- TransLocWidget/src/com/shyamu/translocwidget/MainActivity.java
+++ TransLocWidget/src/com/shyamu/translocwidget/MainActivity.java
@@ -1,18 +1,38 @@
 package com.shyamu.translocwidget;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    Button hideAppIconButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        hideAppIconButton = (Button) findViewById(R.id.bHideAppIcon);
+
+        // On click listener for hide app icon button
+        View.OnClickListener setHideAppIconClickedListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PackageManager p = getPackageManager();
+                p.setComponentEnabledSetting(getComponentName(), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+                Toast.makeText(MainActivity.this,"Icon will be hidden on reboot",Toast.LENGTH_LONG).show();
+            }
+        };
+
+        hideAppIconButton.setOnClickListener(setHideAppIconClickedListener);
 	}
 
 	@Override
