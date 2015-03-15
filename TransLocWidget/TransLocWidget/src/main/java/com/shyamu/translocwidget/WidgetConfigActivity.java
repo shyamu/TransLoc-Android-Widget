0336a62d2e072bfd54d72fe310a16384e811f30b
Shyamal Patel <shyamal.patel14@gmail.com>
2015-03-14 22:52:06 -0400

    theming

--- TransLocWidget/TransLocWidget/src/main/java/com/shyamu/translocwidget/WidgetConfigActivity.java
+++ TransLocWidget/TransLocWidget/src/main/java/com/shyamu/translocwidget/WidgetConfigActivity.java
@@ -1,28 +1,15 @@
 package com.shyamu.translocwidget;
 
 import android.app.Activity;
-import android.app.ListFragment;
-import android.content.Context;
-import android.os.AsyncTask;
-import android.app.FragmentManager;
-import android.support.v7.app.ActionBarActivity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.shyamu.translocwidget.fragments.WidgetListFragment;
 
 
-public class WidgetConfigActivity extends Activity implements WidgetListFragment.OnFragmentInteractionListener{
+public class WidgetConfigActivity extends Activity implements WidgetListFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
